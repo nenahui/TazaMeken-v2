69da9940169dc0a85521497bb74632f02870696c
nenahui <kanatraccoon@gmail.com>
2024-09-15 10:19:21 +0600

    Styles example

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import styles from './header.module.scss';
 
 export const Header: React.FC = () => {
-  return <>Header</>;
+  return <div className={styles.header}>Header</div>;
 };
