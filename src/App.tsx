cc2c49909036d68352760014c129e59724ba83c7
nenahui <kanatraccoon@gmail.com>
2024-09-15 10:25:02 +0600

    Add routers page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Layout } from '@/components/layout/layout';
+import { About } from '@/features/about/about';
 import { Home } from '@/features/home/home';
+import { Merch } from '@/features/merch/merch';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
@@ -8,6 +10,8 @@ export const App: React.FC = () => {
     <Layout>
       <Routes>
         <Route path={'/'} element={<Home />} />
+        <Route path={'/ec-merch'} element={<Merch />} />
+        <Route path={'/about'} element={<About />} />
       </Routes>
     </Layout>
   );
